66bf310fcc5b16c48f390d7bcbf6329677c8b36d
beigestack <m4.sh@tuta.io>
2026-07-09 11:38:08 +0530

    feat: basic login page skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let correctUsername = 'ManikSharma';
+	let correctPassword = 'Manik@123';
+
+	let enteredUsername = $state('');
+	let enteredPassword = $state('');
+
+	function handleLogin() {
+		if (enteredUsername === correctUsername && enteredPassword === correctPassword) {
+			alert('Login successful!');
+		} else {
+			alert('Invalid username or password.');
+		}
+	}
+</script>
+
+<main class="min-h-screen flex flex-col items-center justify-center text-center p-4">
+	<form onsubmit={handleLogin}>
+		<label>
+			Username:
+			<input bind:value={enteredUsername} required />
+		</label>
+		<br />
+		<label>
+			Password:
+			<input type="password" bind:value={enteredPassword} required />
+		</label>
+		<br />
+		<button type="submit">Login</button>
+	</form>
+</main>
